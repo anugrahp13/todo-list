c30477444e0b375325ff6a54b7dd9753f82e4bde
anugrahp13 <anugrahprastyo13@gmail.com>
2024-09-26 22:26:29 +0700

    fix[SelectDeleteAll.jsx]: Bug Fixed for Delete All data and Confirm Message

--- src/components/SelectDeleteAll.jsx
+++ src/components/SelectDeleteAll.jsx
@@ -1,47 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { DeleteModal } from "./DeleteModal";
 
 export const SelectDeleteAll = ({ todos, setTodos }) => {
   const [selectAll, setSelectAll] = useState(false);
-
-  useEffect(() => {
-    // toggle showDeleteAll tampil jika semua data di pilih
-    const allCompleted =
-      todos.length > 0 && todos.every((todo) => todo.completed);
-    setSelectAll(allCompleted);
-  }, [todos]);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const handleSelectAll = () => {
-    const newSelectAll = !selectAll;
-    setSelectAll(newSelectAll);
-
     const updatedTodos = todos.map((todo) => ({
       ...todo,
-      completed: newSelectAll, // set semua data yang di pilih atau tidak
+      completed: !selectAll, // set semua data yang di pilih atau tidak
     }));
+    setSelectAll(!selectAll);
     setTodos(updatedTodos);
   };
 
-  const handleDeleteAll = () => {
-    if (window.confirm("Anda yakin untuk menghapus semua data?")) {
-      setTodos([]); // Hapus semua todos
-      setSelectAll(false); // Reset select all state
-      localStorage.removeItem("todos");
-    }
+  const handleDeleteAllClick = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  if (todos.length === 0) return null;
+  const confirmDeleteAll = () => {
+    setTodos([]); // Hapus semua todos
+    setSelectAll(false); // Reset select all state
+    setIsDeleteModalOpen(false); // tutup modal
+    localStorage.removeItem("todos");
   };
   return (
     <div className="flex justify-between gap-2">
-      <button
-        onClick={handleSelectAll}
-        className="bg-primary text-white text-xs px-3 py-1 rounded-full font-semibold hover:border-primary hover:text-white hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">
-        {selectAll ? "Deselect All" : "Select All"}
-      </button>
-      {selectAll && todos.length > 0 && (
-        <button
-          onClick={handleDeleteAll}
-          className="bg-red-500 text-white text-xs px-3 py-1 rounded-full font-semibold hover:border-primary hover:text-white hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">
-          Delete All
-        </button>
+      {todos.length > 0 && (
+        <>
+          <button
+            onClick={handleSelectAll}
+            className="bg-primary text-white text-xs px-3 py-1 rounded-full font-semibold hover:border-primary hover:text-white hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">
+            {selectAll ? "Deselect All" : "Select All"}
+          </button>
+          <button
+            onClick={handleDeleteAllClick}
+            className="bg-red-500 text-white text-xs px-3 py-1 rounded-full font-semibold hover:border-primary hover:text-white hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">
+            Delete All
+          </button>
+        </>
       )}
+
+      {/* Modal Popup */}
+      <DeleteModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={confirmDeleteAll}
+        title="Delete Data"
+        message="Apakkah kamu yakin ingin menghapus data ini?"
+      />
     </div>
   );
 };
